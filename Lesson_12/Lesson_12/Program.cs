7b2b98e2c92b49c00fa80512c5d364493d9dc7e3
OlgaMermer <meridian127s@gmail.com>
2025-04-13 04:45:07 +0300

    Добавьте файлы проекта.

---
+++ Lesson_12/Lesson_12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lesson_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+        }
+    }
+}
